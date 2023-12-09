a28c744aa89d79b324827a513a0f51bf3722582b
CyzmiX <98268356+CyzmiX@users.noreply.github.com>
2023-12-09 11:32:30 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    preview: {
-    port: 8080,
+  preview: {
+    port: 8000,
   },
 })
